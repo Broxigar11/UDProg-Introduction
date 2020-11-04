a21442531bf6dabc774ee6602cb0d97e9d0d7d30
Broxigar <karafadavid11@gmail.com>
2020-11-04 15:00:50 +0100

    Update drill 6

--- source/Ch06/drill/drill_6.cpp
+++ source/Ch06/drill/drill_6.cpp
@@ -75,8 +75,8 @@ Token Token_stream::get()
     cin >> ch;    // note that >> skips whitespace (space, newline, tab, etc.)
 
     switch (ch) {
-    case ';':    // for "print"
-    case 'q':    // for "quit"
+    case '=':    // for "print"
+    case 'x':    // for "quit"
     case '(': case ')': case '+': case '-': case '*': case '/':
         return Token(ch);        // let each character represent itself
     case '.':
@@ -185,11 +185,17 @@ try
 {
 	double val=0;
 
+	cout << "Welcome to our simple calculator.\nPlease enter expressions using floating-point numbers.\n" << endl;
+	cout << "Available operators: +, -, /, *. To print the result enter the character '='.";
+	cout << "To exit enter character 'x'." << endl;
+
+
+
     while (cin) {
         Token t = ts.get();
 
-        if (t.kind == 'q') break; // 'q' for quit
-        if (t.kind == ';')        // ';' for "print now"
+        if (t.kind == 'x') break; // 'q' for quit
+        if (t.kind == '=')        // ';' for "print now"
             cout << "=" << val << '\n';
         else
             ts.putback(t);
